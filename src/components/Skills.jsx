850aaff11cdc53b7a6c7355038f4054601798bb7
zabidin384 <90741858+zabidin384@users.noreply.github.com>
2022-06-05 23:09:56 +0700

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,8 +11,16 @@ import vue from "../img/imgSkills/vue.png";
 import react from "../img/imgSkills/react.png";
 import next from "../img/imgSkills/next.png";
 import mongodb from "../img/imgSkills/mongodb.png";
+import sass from "../img/imgSkills/sass.png";
+import firebase from "../img/imgSkills/firebase.png";
+import github from "../img/imgSkills/github.png";
+import postman from "../img/imgSkills/postman.png";
+import insomnia from "../img/imgSkills/insomnia.png";
+import replit from "../img/imgSkills/replit.png";
 import { useContext } from "react";
 import { ThemeContext } from "../context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Skills() {
 	const theme = useContext(ThemeContext);
@@ -22,6 +30,12 @@ export default function Skills() {
 		<div className="s" id="skills">
 			<div className="s-wrapper" style={{ backgroundColor: darkMode && "white", color: darkMode && "#222" }}>
 				<div className="s-title">My Skills</div>
+				<p>
+					You can see some of my skill certifications on my Linkedin{" "}
+					<a href="https://www.linkedin.com/in/zainal-tf/" target="_blank" rel="noreferrer">
+						<FontAwesomeIcon icon={faLinkedin} className="linkedIn" />
+					</a>
+				</p>
 				<div className="s-skills">
 					<div className="s-item">
 						<img src={html} alt="" className="s-img" />
@@ -72,6 +86,32 @@ export default function Skills() {
 					<div className="s-item">
 						<img src={mongodb} alt="" className="s-img" />
 						<div className="s-name">MongoDB</div>
+					</div>
+				</div>
+				<div className="s-skills">
+					<div className="s-item">
+						<img src={sass} alt="" className="s-img" />
+						<div className="s-name">SASS</div>
+					</div>
+					<div className="s-item">
+						<img src={firebase} alt="" className="s-img" />
+						<div className="s-name">Firebase</div>
+					</div>
+					<div className="s-item">
+						<img src={github} alt="" className="s-img" />
+						<div className="s-name">Github</div>
+					</div>
+					<div className="s-item">
+						<img src={postman} alt="" className="s-img" />
+						<div className="s-name">Postman</div>
+					</div>
+					<div className="s-item">
+						<img src={insomnia} alt="" className="s-img" />
+						<div className="s-name">Insomnia</div>
+					</div>
+					<div className="s-item">
+						<img src={replit} alt="" className="s-img" />
+						<div className="s-name">Replit</div>
 					</div>
 				</div>
 			</div>

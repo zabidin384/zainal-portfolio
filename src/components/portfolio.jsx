7959b122f43b40bc2ee2db0cf77509bacd35b842
zabidin384 <90741858+zabidin384@users.noreply.github.com>
2022-06-08 10:28:58 +0700

    Add files via upload

---
+++ src/components/portfolio.jsx
@@ -0,0 +1,49 @@
+import "./portfolio.css";
+
+export default function Portfolio() {
+	return (
+		<div id="portfolio">
+			<div className="pl-texts">
+				<h1 className="pl-title">My Portfolio</h1>
+				<p className="pl-desc">Here are some examples of projects I've worked on. Please click the "demo button" to view the full website!</p>
+			</div>
+			<div className="pl-list">
+				<div className="pl-item">
+					<img src="images/projects/zainal-quiz.jpg" alt="" />
+					<div className="pl-name">Who Wants to Be a Millionaire!</div>
+					<a href="https://zainal-quiz.vercel.app/" target="_blank" rel="noreferrer">
+						<button>Demo</button>
+					</a>
+				</div>
+				<div className="pl-item">
+					<img src="images/projects/zainal-candycrush.jpg" alt="" />
+					<div className="pl-name">Candy Crush Game</div>
+					<a href="https://zainal-candycrush.vercel.app/" target="_blank" rel="noreferrer">
+						<button>Demo</button>
+					</a>
+				</div>
+				<div className="pl-item">
+					<img src="images/projects/zainal-2048game.jpg" alt="" />
+					<div className="pl-name">2048 Game</div>
+					<a href="https://zainal-2048.vercel.app/" target="_blank" rel="noreferrer">
+						<button>Demo</button>
+					</a>
+				</div>
+				<div className="pl-item">
+					<img src="images/projects/zainal-flagsmemorygame.jpg" alt="" />
+					<div className="pl-name">Flags Memory Game</div>
+					<a href="https://flagsmemorygame.vercel.app/" target="_blank" rel="noreferrer">
+						<button>Demo</button>
+					</a>
+				</div>
+				<div className="pl-item">
+					<img src="images/projects/zainal-blog.jpg" alt="" />
+					<div className="pl-name">My Blog</div>
+					<a href="https://https://zainal-blog.herokuapp.com//" target="_blank" rel="noreferrer">
+						<button>Demo</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
